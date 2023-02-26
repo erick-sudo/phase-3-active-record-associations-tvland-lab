fcb51455666d363c959903ec5d1918a664615cd1
erick-sudo <erickochieng766@gmail.com>
2023-02-26 22:17:15 +0300

    __

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        Actor.find_by(first_name: self.first_name, last_name: self.last_name)
+    end
+
+    def list_roles
+        characters.map { |character| "'#{self.full_name} - #{}'" }
+    end
 end
