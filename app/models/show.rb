5fc542f123c70662958b8cc218b52767c0f9f563
erick-sudo <erickochieng766@gmail.com>
2023-02-26 23:04:41 +0300

    Passed Test

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
     belongs_to :network
     has_many :characters
     has_many :actors, through: :characters
+
+    def actors_list
+        actors.map(&:full_name)
+    end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,6 +3,9 @@ class CreateCharacters < ActiveRecord::Migration[6.1]
     # Your code here
     create_table :characters do |t|
       t.string :name
+
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
